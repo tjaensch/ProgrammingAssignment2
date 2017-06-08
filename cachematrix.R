821ff71c17f66d8e43b31c60519afc93419de3d8
Thomas Jaensch <thomasjaensch@gmail.com>
2017-06-08 13:53:31 -0400

    Completed function stubs

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Rewrote given function to take matrix argument instead of numeric vector
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m_inverse <- NULL
+    set <- function(y) {
+        x <<- y;
+        m_inverse <<- NULL;
+    }
+    get <- function() return(x);
+    setinverse <- function(inv) m_inverse <<- inv;
+    getinverse <- function() return(m_inverse);
+    return(list(set = set, get = get, setinverse = setinverse, getinverse = getinverse))
 }
 
 
-## Write a short comment describing this function
+## Rewrote given function to match variable names in makeCacheMatrix function above
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m_inverse <- x$getinverse()
+    if(!is.null(m_inverse)) {
+        message("Getting cached data...")
+        return(m_inverse)
+    }
+    data <- x$get()
+    m_inverse <- solve(data, ...)
+    x$setinverse(m_inverse)
+    m_inverse
 }
